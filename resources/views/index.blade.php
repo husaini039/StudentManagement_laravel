67532e8e9ee39a889892813bf6f65220d9aed37a
husaini039 <108285286+husaini039@users.noreply.github.com>
2025-08-03 17:39:04 +0800

    learning laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=h1, initial-scale=1.0">
     <title>Document</title>
-
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
     <h1>sani welcossme</h1>
+
+    <a href="/testpage">Go to Test</a>
+
+    <form action="{{ route('formsubmit') }}" method="POST">
+        @csrf
+        <input type="text" name="name" placeholder="Enter your name" required>
+        <button type="submit">Submit</button>
+    </form>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('index');
 });
+
+//test route
+Route::get('/testpage', function () {
+    return "i swear this is a test page";
+});
+
+//post route example
+Route::post('/formsubmit', function (Request $request) {
+//validate the data input
+$request->validate([
+        'name' => 'required|string|min:5|max:10',
+    ]);
+
+    $name = $request->input('name');
+    return "name is $name";
+})->name('formsubmit');
